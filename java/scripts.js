9575ba2c3f2f530eb6967451f59b8460bcc3069e
Brandon Grissom <lousaydus@gmail.com>
2017-07-08 00:02:09 -0700

    Initial commit

---
+++ java/scripts.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+//Global variables
+    var tryCount = 10;
+    var winPhrase = 'This is a really long test string.';
+    var phraseLetters = [];
+    var phraseLettersDivID = [];
+    var gameState = 'ready';
+                  
+    (winPhrase.indexOf('a')>-1) ? console.log('Yes') : console.log('No');
+                  
+//get all the letters in the target phrase and then create that many divs inside of the phrase container
+//then populate the divs with either an underscore or a letter if that div has been clicked.
+$('.gameStartButton').click(function(){
+    console.log(gameState);
+    switch(gameState){
+            
+        case 'ready':
+            gameState = 'busy';
+            for(i=0; i < winPhrase.length; i++){
+                setTimeout(function(){
+                $('.phraseContainer').append('<div class="phraseLetterDiv" id="phraseLetterID' + i + '">_</div>');
+                }, i*100);
+            }
+            
+            setTimeout(function(){
+                var allLetterDivs = $('.phraseLetterDiv');
+                
+                for(i=0; i < winPhrase.length; i++){
+                    $(allLetterDivs[i]).attr('id', 'phraseLetterID'+i);
+                }
+                gameState = 'inplay';
+            }, ((winPhrase.length)*100)+50);
+            
+            $('.gameStartButton').css('background-color', 'red');
+        break;
+            
+        case 'inplay':
+            for(i=0; i < $('.phraseLetterDiv').length; i++){
+                phraseLettersDivID.push('#phraseLetterID' + i);
+                console.log(phraseLettersDivID[i]);
+                $(phraseLettersDivID[i]).css('background-color', 'yellow');
+                
+            }
+            //remove all the divs. Ready gamestate.
+            for(i=0; i < winPhrase.length; i++){
+                $('#phraseLetterID'+i).remove();
+                }
+
+            gameState = 'ready';
+        break;
+            
+        case 'busy':
+        break;
+        
+        default:
+        break;
+    }
+
+});
+    
+    
+  
+});
